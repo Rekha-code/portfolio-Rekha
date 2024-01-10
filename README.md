43454e678cf84e8b73166c2bf24eaeeb12a23658
rekha_gorde <rekha.gorde8@gmail.com>
2024-01-10 23:17:21 +0530

    rekha-portfolio

---
+++ README.md
@@ -0,0 +1,9 @@
+# output
+
+![image](https://github.com/Rekha-code/rekhaportfolio/assets/76820881/66793c93-766c-4857-959b-fdb284249500)
+
+![image](https://github.com/Rekha-code/rekhaportfolio/assets/76820881/75dbdc3f-b31f-430f-a5d3-744d19203a8d)
+
+![image](https://github.com/Rekha-code/rekhaportfolio/assets/76820881/5cbebc8e-b95b-4346-9ab7-d32ee3a99694)
+
+
---
+++ src/data/constants.js
@@ -0,0 +1,301 @@
+export const Bio = {
+  name: "Rekha Gorde",
+  roles: [
+    "Full Stack Developer",
+    "Front End Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Rekha-code",
+  resume:
+    "https://azure-blondell-35.tiiny.site/",
+  linkedin: "https://www.linkedin.com/in/rekha-suhas-gorde-a66732221/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+  
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Software Developer",
+    company: "The Whiteborad Company.",
+    date: "Sep 2021 - Oct 2022",
+    desc: ".Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Laravel",
+      "React Js",
+      "Bootstrap",
+      "MySql",
+    ],
+  },
+  {
+    id: 0,
+    role: "Web Developer",
+    company: "Itransparity ",
+    date: "June 2020 - 2021",
+    desc: "Working on Itransparity Platforms, Design and developed user-friendly websites in HTML,CSS,Bootstrap,ReactJS, Laravel and Mysql",
+    skills: [
+      "ReactJS",
+      "Laravel",
+      "Bootstrap",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MySql",
+    ],
+  },
+  {
+    id: 1,
+    role: "Web Developer",
+    company: "Ideastack Solutions Pvt Ltd ",
+    date: "June 2019 - July 2020",
+    desc: "Maintain and enhanced existing web panels of company.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Laravel",
+      "MySql",
+    ],
+  },
+  {
+    id: 2,
+    role: "Front End Developer ",
+    company: "Ideatore Pvt Ltd.",
+    date: "May 2018- 2019",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "MySql",
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "Bharati Vidyapeeth' Institute of Management & information Technology",
+    date: "June 2011 - July 2015",
+    grade: "7.28 CGPA",
+    desc: "I completed Master of Computer Application at Bharati Vidyapeeth' Institute of Management & information Technology.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    school: "Women's College of Home Science and BCA.PKVM Campus",
+    date: "Apr 2009 - Apr 2011",
+    grade: "66.7%",
+    desc: "I completed Master of Computer Application  at Women's College of Home Science and BCA.PKVM Campus, Babhaleshwar Road, Loni-413713, Dist-Ahmednagar (Maharashtra) India.",
+    degree: "Bachelor of Computer Application",
+  },
+  {
+    id: 2,
+    school: "SSGM college, kopargaon",
+    date: "Apr 2007 - Apr 2008",
+    grade: "54.33%",
+    desc: "I completed my class 12 education at SSGM college, kopargaon.",
+    degree: "Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "HRM Management System ",
+    description:
+      "HRM Management System  is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently.",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Laravel",
+      "MySql",
+    ],
+    category: "web app",
+    webapp:" https://hrm.ideastack.com",
+  },
+  {
+    id: 1,
+    title: "Indocount",
+    description:
+      "Indo Count is a specialized end-to-end bedding provider that is solely focused on creating all-encompassing sleep experiences. Our passion and global vision have allowed us to enter 49 markets with the objective of continuing to add more every year. We are also the preferred partner for some of the best-known retail, hospitality and fashion brands in the world. We are fast, agile, and respond to the needs of clients with solutions that make a difference in consumers’ lives.",
+    tags: ["Wordpress", "HTML", "CSS", "Bootstrap",],
+    category: "web app",
+    webapp: "https://www.indocount.com/",
+  },
+  {
+    id: 2,
+    title: "ERP",
+    description:
+      "ERP systems can improve efficiency and streamline operations by providing real-time data and automated workflows. The implementation of an ERP system typically requires extensive planning, training, and integration with existing systems. Successful ERP projects require effective communication, clear project goals, and a strong team.",
+    tags: ["HTML", "CSS","Bootstrap","Javascript","Laravel"],
+    category: "web app",
+    webapp: "http://erp.itransparity.com",
+  },
+  {
+    id: 3,
+    title: "Ctrl Visa",
+    description:
+      "A control visa project refers to a detailed plan or program designed to ensure effective management and monitoring of visa applications and issuance. The goal of the project is to minimize fraud, streamline the visa application process, and improve overall security.",
+    tags: ["HTML", "CSS", "Bootstrap", "Javascript","Laravel","Mysql"],
+    category: "web app",
+    webapp: "https://uat.ctrlvisa.com",
+  },
+  {
+    id: 10,
+    title: "Hotel-website",
+    description:
+      "These websites are typically created using HTML, CSS, and JavaScript, which are three fundamental technologies used in web development.",
+    tags: ["HTML","CSS","Bootstrap", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/Rekha-code/hotel-single-page-website",
+    webapp: "https://hotel-single-page-website.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Card Filter Website",
+    description:
+      "A card filter website using HTML, CSS, and JavaScript is a web application that allows users to filter, sort, and display data using cards.",
+    tags: ["HTML", "CSS","Bootstrap", "Javascript",],
+    category: "web app",
+    github: "https://github.com/Rekha-code/card-filter-website-htmlcssjs",
+    webapp: "https://rekha-code.github.io/card-filter-website-htmlcssjs/",
+  },
+  {
+    id: 5,
+    title: "Todo Weadther App",
+    description:
+      " A Todo Web App made with React JS. users can create a new task. It has a search bar where users can search for a particular task.",
+    tags: ["React Js", "HTML", "CSS", "Bootstrap"],
+    category: "web app",
+    github: "https://github.com/Rekha-code/todo-Weather-react",
+    webapp: "https://rekha-code.github.io/todo-Weather-react/",
+  },
+  {
+    id: 6,
+    title: "Restaurant",
+    description:
+      "A simple react app Restaurant that allows users to order food  from a restaurant.",
+    tags: ["React Js", "API", "Axios"],
+    category: "web app",
+    github: "https://github.com/Rekha-code/restaurant-incrdecre-react",
+    webapp: "https://rekha-code.github.io/restaurant-incrdecre-react/",
+  },
+  {
+    id: 7,
+    title: "Bakery",
+    description:
+      "Baker's Kitchen is an online platform that specializes in offering kitchen essentials and appliances. It features a simple and user-friendly website built on HTML, CSS, and JavaScript technologies.",
+    tags: ["HTML", "CSS", "Javascript"],
+    category: "web app",
+    github: "https://github.com/Rekha-code/Bakery-website-HtmlCssJs",
+    webapp: "https://rekha-code.github.io/Bakery-website-HtmlCssJs/",
+  },
+  // {
+  //   id: 8,
+  //   title: "Face Recognition",
+  //   date: "Jan 2021",
+  //   description:
+  //     "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+  //   image:
+  //     "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
+  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+  //   category: "machine learning",
+  //   github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  //   webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  // },
+];
+
